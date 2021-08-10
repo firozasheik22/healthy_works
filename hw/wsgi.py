21ac1ae84ad2dba8611fe47197eab8e562bc5022
firozasheik22 <firozasheik22@gmail.com>
2021-08-10 23:11:18 +0530

    fourth commit

--- hw/wsgi.py
+++ hw/wsgi.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
+port = int(os.environ.get("PORT", 5000))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hw.settings')
 
 application = get_wsgi_application()
+
+
